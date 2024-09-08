e40d5fd5c7b4ee4b6b0155b54851af2eaa1ced5a
blackbrokkoli <kastomat@gmail.com>
2024-09-08 15:33:37 +0300

    print basic card info

--- __init__.py
+++ __init__.py
@@ -5,18 +5,37 @@ from aqt.utils import showInfo, qconnect
 # import all of the Qt GUI library
 from aqt.qt import *
 
+from anki.cards import Card
+
 # We're going to add a menu item below. First we want to create a function to
 # be called when the menu item is activated.
 
 def testFunction() -> None:
-    # for each card in the collection
-    for card in mw.col.find_cards(""):
-        print(card)
-    # get the number of cards in the current collection, which is stored in
-    # the main window
-    cardCount = mw.col.cardCount()
-    # show a message box
-    showInfo("Card count: %d" % cardCount)
+    col = mw.col
+    card_ids = col.db.list("SELECT id FROM cards")
+
+    # Function to check if a card is due
+    def is_card_due(card: Card) -> bool:
+        # Card is due if it's in the review queue and due <= today
+        return card.queue == 2 and card.due <= col.sched.today
+
+    # Iterate over the card IDs and get learning stats
+    for card_id in card_ids:
+        card = col.get_card(card_id)  # Get the Card object
+
+        due_status = is_card_due(card)
+        interval = card.ivl  # Interval (days until next review)
+        reps = card.reps  # Number of repetitions
+        ease = card.factor / 1000  # Ease factor
+
+        if due_status:
+
+            print(f"Card ID: {card_id}")
+            print(f"Due: {due_status}")
+            print(f"Interval: {interval} days")
+            print(f"Repetitions: {reps}")
+            print(f"Ease: {ease}")
+            print("----------")
 
 # create a new menu item, "test"
 action = QAction("test", mw)
